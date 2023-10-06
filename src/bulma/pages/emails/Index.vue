65f499e80c2559549e2776f0e93b2e6c6b0f2b71
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-06 15:59:20 +0000

    Replace enso-ui to liberu-ui

--- src/bulma/pages/emails/Index.vue
+++ src/bulma/pages/emails/Index.vue
@@ -20,7 +20,7 @@
 <script>
 
 import { mapState } from 'vuex';
-import { EnsoTable } from '@enso-ui/tables/bulma';
+import { EnsoTable } from '@liberu-ui/tables/bulma';
 
 export default {
     name: 'Index',
